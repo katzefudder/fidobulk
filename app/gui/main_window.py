577026f08903c894fc523fdfb5b53eacf0e5a9fe
Florian Dehn <florian.dehn@cgi.com>
2025-04-28 14:21:51 +0200

    feature: setting a random pin

--- app/gui/main_window.py
+++ app/gui/main_window.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout, QPlainTextEdit, QPushButton, QComboBox, QMessageBox, QLineEdit
+from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout, QPlainTextEdit, QPushButton, QComboBox, QMessageBox, QLineEdit, QDialog
 from lib.device import Device
 from lib.auth import get_access_token
 from lib.users import fetch_users
@@ -7,6 +7,7 @@ from lib.api_client import submit_user_data
 class YkBatch(QWidget):
 
     device = []
+    random_pin = "00000"
 
     def __init__(self):
         super().__init__()
@@ -30,45 +31,70 @@ class YkBatch(QWidget):
         layout.addWidget(self.label)
 
         self.access_token = get_access_token("clientId", "secret", "domainName.onmicrosoft.com")
-        self.access_token_text = QPlainTextEdit(self.access_token)
-        self.access_token_text.setReadOnly(True)
-        layout.addWidget(self.access_token_text)
 
-        self.users = fetch_users(self.access_token)
-        self.user_combobox = QComboBox()
-        for user in self.users:
-            self.user_combobox.addItem(user['displayName'], user)
-        layout.addWidget(self.user_combobox)
+        if device_instance.pin_already_set:
+            self.pin_text_label = QLabel("Pin schon gesetzt. Um diesen neu zu setzen, bitte FIDO-Stick zurücksetzen.")
+            layout.addWidget(self.pin_text_label)
+        else:
+            self.users = fetch_users(self.access_token)
+            self.user_combobox = QComboBox()
+            for user in self.users:
+                self.user_combobox.addItem(user['displayName'], user)
+            layout.addWidget(self.user_combobox)
+            self.set_random_pin = QPushButton("Neue zufällige PIN setzen")
+            self.set_random_pin.clicked.connect(self.handle_set_random_pin)
+            layout.addWidget(self.set_random_pin)
 
-        self.pin_text_label = QLabel("Pin eingeben:")
-        layout.addWidget(self.pin_text_label)
-        self.pin_text_box = QLineEdit(self)
-        self.pin_text_box.setMaxLength(8)
-        self.pin_text_box.move(20, 20)
-        self.pin_text_box.resize(280,40)
-        layout.addWidget(self.pin_text_box)
+            # self.send_button = QPushButton("Absenden")
+            # self.send_button.clicked.connect(self.handle_submit)
+            # layout.addWidget(self.send_button)
 
-        self.reset_button = QPushButton("Reset Fido Stick")
-        self.reset_button.clicked.connect(self.handle_reset)
-        layout.addWidget(self.reset_button)
-
-        self.send_button = QPushButton("Absenden")
-        self.send_button.clicked.connect(self.handle_submit)
-        layout.addWidget(self.send_button)
+        # self.reset_button = QPushButton("Reset Fido Stick")
+        # self.reset_button.clicked.connect(self.handle_reset)
+        # layout.addWidget(self.reset_button)
 
         self.setLayout(layout)
 
     def handle_submit(self):
         selected_user = self.user_combobox.currentData()
-        pin_value = self.pin_text_box.text()
-        print(f"pin: {pin_value}")
+        # TODO: update GUI after successfully setting the pin
         try:
-            self.device.set_pin(pin_value)
-            submit_user_data(selected_user, pin_value, self.serial_number)
-            QMessageBox.information(self, "Erfolg", "✅ Nachricht gesendet!")
+            submit_user_data(selected_user, self.random_pin, self.serial_number)
+            QMessageBox.information(self, "Erfolg", f"✅ Pin für {selected_user.displayName} gesetzt und zur Datenbank übermittelt!")
         except Exception as e:
             QMessageBox.critical(self, "Fehler", f"⛔️ Fehler:\n{e}")
 
-    def handle_reset(self):
-        print(f"reset device {self.device}")
-        self.device.reset()
+    def handle_set_random_pin(self):
+        pin_value = self.device.generate_pin()
+        print(f"pin set: {pin_value}")
+        # TODO: update GUI after successfully setting the pin
+        try:
+            self.random_pin = pin_value
+            self.device.set_pin(pin_value)
+            QMessageBox.information(self, "Erfolg", "✅ PIN erfolgreich gesetzt: " + str(pin_value))
+            self.set_random_pin.hide()
+            
+            try:
+                selected_user = self.user_combobox.currentData()
+                submit_user_data(selected_user, self.random_pin, self.serial_number)
+                QMessageBox.information(self, "Erfolg", f"✅ Pin für {selected_user} gesetzt und zur Datenbank übermittelt!")
+                self.user_combobox.hide()
+            except Exception as e:
+                QMessageBox.critical(self, "Fehler", f"⛔️ Fehler:\n{e}")
+
+        except Exception as e:
+            QMessageBox.critical(self, "Fehler", f"⛔️ Fehler:\n{e}")
+
+    # def handle_reset(self):
+    #     reply = QMessageBox.question(
+    #         self, 'Reset bestätigen', 
+    #         "Der Fido Stick wird zurückgesetzt: sicher?",
+    #         QMessageBox.Yes | QMessageBox.No, QMessageBox.No
+    #     )
+    #     if reply == QMessageBox.Yes:
+    #         # Perform reset actions
+    #         QMessageBox.critical(self, "Info", f"Bitte den Fidokey berühren, halten und OK klicken")
+    #         self.device.reset()
+    #         QMessageBox.information(self, "Reset", "Der Fido Stick wurde zurückgesetzt.")
+
+        
--- app/lib/device.py
+++ app/lib/device.py
@@ -3,11 +3,19 @@ from fido2.hid import CtapHidDevice
 from fido2.ctap2 import Ctap2
 from fido2.ctap2.pin import ClientPin
 from fido2.ctap import CtapError
+import secrets
+import string
 import threading
 
 class Device:
+    device = {}
+    pin_already_set = False
+
     def __init__(self):
         self.device = self._find_device()
+        self.pin_already_set = self.pin_already_set()
+        print("Pin already set: " + str(self.pin_already_set))
+
 
     def _find_device(self):
         device = next(CtapHidDevice.list_devices(), None)
@@ -35,6 +43,8 @@ class Device:
         try:
             ctap2 = Ctap2(self.device)
             ctap2.reset()  # This call blocks until reset is complete
+            dev = self.wait_for_device()
+            print(f"dev: {dev}")
             print("✅ Device has been reset successfully.")
             event.set()
         except CtapError as e:
@@ -43,13 +53,30 @@ class Device:
             else:
                 print(f"❌ Reset failed: {e}")
 
+    def wait_for_device(self, timeout=10):
+        """Wait for a FIDO device to be available again."""
+        start = time.time()
+        while time.time() - start < timeout:
+            devices = list(CtapHidDevice.list_devices())
+            if devices:
+                return devices[0]
+            time.sleep(0.2)
+        raise TimeoutError("Timeout waiting for FIDO device to reappear.")
+
     def reset(self):
         reset_success = threading.Event()
         reset_thread = threading.Thread(target=self._reset_device, args=(reset_success,))
         reset_thread.start()
 
+    def pin_already_set(self):
+        ctap2 = Ctap2(self.device)
+        return ctap2.info.options.get("clientPin")
+
     def set_pin(self, new_pin):
         ctap2 = Ctap2(self.device)
+        if ctap2.info.options.get("clientPin"):
+            print("\tPIN already set for the device")   # TODO: should display a hint in GUI just in case PIN has already been set
+            quit()
         client_pin = ClientPin(ctap2)
 
         try:
@@ -58,3 +85,35 @@ class Device:
             print(f"❌ Fehler beim Setzen der PIN: {e}")
         
         return
+
+    def set_pin_minimum_length(self):
+        ctap = Ctap2(self.device)
+        if ctap.info.options.get("setMinPINLength"):
+            client_pin = ClientPin(ctap)
+            token = client_pin.get_pin_token(
+                pin, ClientPin.PERMISSION.AUTHENTICATOR_CFG
+            )
+            config = Config(ctap, client_pin.protocol, token)
+            print("\tGoing to set the minimum pin length to 6.")
+            config.set_min_pin_length(min_pin_length=6)
+            print("\tGoing to force a PIN change on first use.")
+            config.set_min_pin_length(force_change_pin=True)
+
+    def generate_pin(self):
+        disallowed_pins = [
+            "123456",
+            "123123",
+            "654321",
+            "123321",
+            "112233",
+            "121212",
+            "520520",
+            "123654",
+            "159753",
+        ]
+
+        while True:
+            digits = "".join(secrets.choice(string.digits) for _ in range(6))
+            # Check if PIN is not trivial and not in banned list
+            if len(set(digits)) != 1 and digits not in disallowed_pins:
+                return digits
